3b249fb14c053191e5100180e5d55a3ace0cb14b
nikobothiuse <nikogifthouse@gmail.com>
2025-01-25 17:55:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   BOT_TOKEN: "7743524884:AAFmNKa80Ls3K4U0ern-3yvU7xg8fvIT1iw", // Token bot Telegram
-  OWNER_ID: "7806795757", // ID pemilik bot
+  OWNER_ID: "8105186518", // ID pemilik bot
   allowedGroupIds: [-1002401306074, -1002361556670, -1002386380830, -1002415460020, -4514397992], // ID grup yang diizinkan
 };
